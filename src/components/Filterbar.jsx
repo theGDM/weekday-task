00c8ba1c841973d4b1faf430e4ffe4f8695c3fa1
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-05-03 10:23:57 +0530

    Gyandeep | Created data directory to store all the options for select menu

--- src/components/Filterbar.jsx
+++ src/components/Filterbar.jsx
@@ -2,7 +2,9 @@ import { Box, Chip, FormControl, InputBase, InputLabel, MenuItem, OutlinedInput,
 import React, { useState } from 'react'
 import { createStyles, makeStyles, styled, useTheme } from '@mui/styles';
 import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
-
+import { useDispatch, useSelector } from "react-redux";
+import { SetRole } from '../actions/FilterAction';
+import { DesignRoles, EngineeringRoles, ProductRoles } from '../data/FilterOptionsList';
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -23,8 +25,8 @@ const useStyles = makeStyles((theme) =>
             flexWrap: "wrap"
         },
         chip: {
-            margin: 2,
-            backgroundColor: "#FFF"
+            backgroundColor: "#FFF",
+            padding: '0.2rem 0.2rem'
         },
         noLabel: {
             marginTop: theme.spacing(3)
@@ -34,28 +36,16 @@ const useStyles = makeStyles((theme) =>
 
 const Filterbar = () => {
     const classes = useStyles();
-    const ITEM_PADDING_TOP = 8;
+    let dispatch = useDispatch();
+
     const MenuProps = {
         PaperProps: {
             style: {
-                maxHeight: 38 * 4.5 + ITEM_PADDING_TOP,
+                maxHeight: '30rem',
                 width: '10rem',
             },
         },
     };
-
-    const names = [
-        'Oliver Hansen',
-        'Van Henry',
-        'April Tucker',
-        'Ralph Hubbard',
-        'Omar Alexander',
-        'Carlos Abbott',
-        'Miriam Wagner',
-        'Bradley Wilkerson',
-        'Virginia Andrews',
-        'Kelly Snyder',
-    ];
 
     const BootstrapInput = styled(InputBase)(({ theme }) => ({
         '& .MuiInputBase-input': {
@@ -65,6 +55,7 @@ const Filterbar = () => {
             border: `0.15rem solid rgb(204, 204, 204)`,
             borderRadius: '0.4rem',
             fontSize: '13px',
+            height: '15rem',
             padding: '0.8rem 0.8rem 0.8px 0.8rem',
             "&:hover": {
                 borderRadius: '0.4rem',
@@ -81,14 +72,16 @@ const Filterbar = () => {
 
     const theme = useTheme();
     const [role, setRole] = useState([]);
+
     const handleChange = (e) => {
         setRole(e.target.value);
+        dispatch(SetRole(e.target.value));
     };
 
     return (
 
         <Box display='flex' flexDirection='row' alignItems='center' flexWrap='wrap'>
-            <FormControl sx={{ m: 1, width: 300 }}>
+            <FormControl sx={{ m: 1, width: '20rem' }} size='medium'>
                 <InputLabel id="select-role"
                     sx={{
                         fontSize: '1.5rem',
@@ -106,11 +99,6 @@ const Filterbar = () => {
                     value={role}
                     onChange={handleChange}
                     input={< BootstrapInput />}
-                    sx={{
-                        height: '4rem',
-                        width: '15rem',
-                        transition: 'all 100ms ease 0s'
-                    }}
                     renderValue={(selected) => (
                         <Chip
                             sx={{
@@ -137,17 +125,156 @@ const Filterbar = () => {
                     MenuProps={MenuProps}
                 >
                     <MenuItem
-                        sx={{ fontSize: '1.4rem', fontWeight: '300' }}
-                        disabled value="">
+                        sx={{
+                            fontSize: '1.4rem', fontWeight: '400', backgroundColor: '#fff'
+                        }}
+                        disabled
+                        value=""
+                    >
                         ENGINEERING
                     </MenuItem>
-                    {names.map((name) => (
+                    {EngineeringRoles.map((erole) => (
                         <MenuItem
-                            key={name}
-                            value={name}
+                            key={erole}
+                            value={erole}
                             sx={{ fontSize: '1.4rem', fontWeight: '300' }}
                         >
-                            {name}
+                            {erole}
+                        </MenuItem>
+                    ))}
+                    <MenuItem
+                        sx={{
+                            fontSize: '1.4rem', fontWeight: '400', backgroundColor: '#fff'
+                        }}
+                        disabled
+                        value=""
+                    >
+                        DESIGN
+                    </MenuItem>
+                    {DesignRoles.map((drole) => (
+                        <MenuItem
+                            key={drole}
+                            value={drole}
+                            sx={{ fontSize: '1.4rem', fontWeight: '300' }}
+                        >
+                            {drole}
+                        </MenuItem>
+                    ))}
+                    <MenuItem
+                        sx={{
+                            fontSize: '1.4rem', fontWeight: '400', backgroundColor: '#fff'
+                        }}
+                        disabled
+                        value=""
+                    >
+                        PRODUCT
+                    </MenuItem>
+                    {ProductRoles.map((prole) => (
+                        <MenuItem
+                            key={prole}
+                            value={prole}
+                            sx={{ fontSize: '1.4rem', fontWeight: '300' }}
+                        >
+                            {prole}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <FormControl sx={{ m: 1, width: '20rem' }} size='medium'>
+                <InputLabel id="select-role"
+                    sx={{
+                        fontSize: '1.5rem',
+                        color: '#000',
+                        fontWeight: '400'
+                    }}
+
+                >
+                    Roles
+                </InputLabel>
+                <Select
+                    labelId="select-role"
+                    id="single-select-role"
+                    select
+                    value={role}
+                    onChange={handleChange}
+                    input={< BootstrapInput />}
+                    renderValue={(selected) => (
+                        <Chip
+                            sx={{
+                                borderRadius: '0.4rem',
+                                padding: '0',
+                                fontSize: '1.2rem',
+                                fontWeight: '300'
+                            }}
+                            className={classes.chip}
+                            clickable
+                            deleteIcon={
+                                <ClearRoundedIcon
+                                    color='#000'
+                                    fontSize='0.5rem'
+                                    onMouseDown={(event) => event.stopPropagation()}
+                                />
+                            }
+                            onDelete={() => console.log('delteed')}
+                            onClick={() => setRole('')}
+                            key={role}
+                            label={role}
+                        />
+                    )}
+                    MenuProps={MenuProps}
+                >
+                    <MenuItem
+                        sx={{
+                            fontSize: '1.4rem', fontWeight: '400', backgroundColor: '#fff'
+                        }}
+                        disabled
+                        value=""
+                    >
+                        ENGINEERING
+                    </MenuItem>
+                    {EngineeringRoles.map((erole) => (
+                        <MenuItem
+                            key={erole}
+                            value={erole}
+                            sx={{ fontSize: '1.4rem', fontWeight: '300' }}
+                        >
+                            {erole}
+                        </MenuItem>
+                    ))}
+                    <MenuItem
+                        sx={{
+                            fontSize: '1.4rem', fontWeight: '400', backgroundColor: '#fff'
+                        }}
+                        disabled
+                        value=""
+                    >
+                        DESIGN
+                    </MenuItem>
+                    {DesignRoles.map((drole) => (
+                        <MenuItem
+                            key={drole}
+                            value={drole}
+                            sx={{ fontSize: '1.4rem', fontWeight: '300' }}
+                        >
+                            {drole}
+                        </MenuItem>
+                    ))}
+                    <MenuItem
+                        sx={{
+                            fontSize: '1.4rem', fontWeight: '400', backgroundColor: '#fff'
+                        }}
+                        disabled
+                        value=""
+                    >
+                        PRODUCT
+                    </MenuItem>
+                    {ProductRoles.map((prole) => (
+                        <MenuItem
+                            key={prole}
+                            value={prole}
+                            sx={{ fontSize: '1.4rem', fontWeight: '300' }}
+                        >
+                            {prole}
                         </MenuItem>
                     ))}
                 </Select>
---
+++ src/data/FilterOptionsList.js
@@ -0,0 +1,36 @@
+export const EngineeringRoles = [
+    'Backend',
+    'Frontend',
+    'Fullstack',
+    'Ios',
+    'Flutter',
+    'React Native',
+    'Android',
+    'Tech Lead',
+    'Dev-Ops',
+    'Data Engineer',
+    'Data Sceince',
+    'Computer Vision',
+    'Nlp',
+    'Deep Learning',
+    'Test / QA',
+    'Web3',
+    'SRE',
+    'Data-Infrastructure'
+];
+
+export const DesignRoles = [
+    'Designer',
+    'Designer Manager',
+    'Graphic Designer',
+    'Product Designer'
+]
+
+export const ProductRoles = [
+    'Product Manger'
+]
+
+export const OperationRoles = [
+    'Operation Manager',
+    'Founder\'s office / Chief of Staff'
+]
